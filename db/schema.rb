4404c53fa6a8b2a83f6a9ed1b4c849a22a3f3782
Sergey Romanov <judge_exe@mail.ru>
2019-03-17 17:37:19 +0300

    add model Status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_132718) do
+ActiveRecord::Schema.define(version: 2019_03_17_143002) do
 
   create_table "orders", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
