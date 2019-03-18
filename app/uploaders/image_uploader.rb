654bad32cc03ba86a2b7e4c74ac5001e507acfbd
Romanov Sergey <judge_exe@mail.ru>
2019-03-18 10:40:03 +0300

    add currency and hide select status for new order

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [150, 150]
    end
    version :large do
-    process resize_to_fit: [250, 250]
+    process resize_to_fit: [350, 350]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
